2f7932784d119bbbfa24f63f5a06ba9fb32b0ed3
KobusPitzer <kobus.pitzer@gmail.com>
2017-10-10 13:42:22 +0200

    [SCREEN] Fixed a bug where on some screen dimensions no information was shown

--- app/src/main/java/com/jooksu/kobusp/pocketweather/MainActivity.java
+++ app/src/main/java/com/jooksu/kobusp/pocketweather/MainActivity.java
@@ -16,6 +16,8 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
@@ -86,6 +88,10 @@ public class MainActivity extends AppCompatActivity implements
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_main);
         setupShakeSensor();
         setupCelsiusJoke();
